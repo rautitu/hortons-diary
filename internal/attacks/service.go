ea126711c36a95b903c0ee10b1534e7b133dd3ea
Tuomas Rautiola <tuomas.rautiola@gmail.com>
2026-09-20 12:24:04 +0300

    work on service continued (initiated auth too)

--- internal/attacks/service.go
+++ internal/attacks/service.go
@@ -1,8 +1,24 @@
 package attacks
 
+import (
+	"context"
+	"time"
+
+	"github.com/tonnomolt/hortons-diary/internal/auth"
+)
+
 type Repository interface {
-	//Create(ctx context.Context, userID UserID,
-	//	input CreateInput) (Attack, error)
+	Create(
+		ctx context.Context,
+		userID auth.UserID,
+		record PainRecord,
+	) (PainRecord, error)
+}
+
+type CreateInput struct {
+	StartTime       time.Time
+	DurationMinutes int
+	Severity        int
 }
 
 type Service struct {
@@ -13,10 +29,19 @@ func NewService(repo Repository) *Service {
 	return &Service{repo: repo}
 }
 
-//func (s *Service) Create(
-//    ctx context.Context,
-//    userID UserID,
-//    input CreateInput,
-//) (Attack, error) {
-//    return s.repo.Create(ctx, userID, input)
-//}
+func (s *Service) Create(
+	ctx context.Context,
+	userID auth.UserID,
+	input CreateInput,
+) (PainRecord, error) {
+	record, err := NewPainRecord(
+		input.StartTime,
+		input.Severity,
+		input.DurationMinutes,
+	)
+	if err != nil {
+		return PainRecord{}, err
+	}
+
+	return s.repo.Create(ctx, userID, record)
+}
